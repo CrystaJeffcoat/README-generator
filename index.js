a4ade6adb49590fd37ef90cbc7f8551d163d84e8
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-07-02 18:21:34 -0400

    function changed to write to readme.md file all at once.

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 const readmeQuestions = require("./readmeGen.js");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const { keys } = require("./readmeGen.js");
 
 let username;
 let reponame;
-const queryURL = `https://api.github.com/repos/${username}/${reponame}/contents/`
 
 // Questions to retrieve username and repository name for github--
 const userQuestions = [
@@ -24,66 +25,46 @@ const userQuestions = [
 ];
 
 // Get user info for github API--
-getRepo();
-async function getRepo() {
-    try {
-        await inquirer
-        .prompt(userQuestions)
-        .then(function(res) {
-            if ((res.username) && (res.reponame)) {
-                username = res.username
-                reponame = res.reponame
-            }else {
-                console.log("Enter a valid username and repo");
-            }
-        });
+// getRepo();
+// async function getRepo() {
+//     try {
+//         await inquirer
+//         .prompt(userQuestions)
+//         .then(function(res) {
+//             if ((res.username) && (res.reponame)) {
+//                 username = res.username
+//                 reponame = res.reponame
+//             }else {
+//                 console.log("Enter a valid username and repo");
+//             }
+//         });
     
-        const { data } = await axios.get(queryURL);
-    
-        console.log(data[0].name);
-    
-    } catch (err) {
-        console.log(err);
-    }
-}
+//         const { data } = await axios.get(`https://api.github.com/repos/${username}/${reponame}/contents/`);
+//         console.log(data[0].name);
+//         getData();
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
 
-// Asks initial questions from readmeGen file--
+// Asks initial questions from readmeGen file-- Gets outline for readme
+let formattedData = []
 getData();
 async function getData() {
     try {
         await inquirer
-            .prompt(readmeQuestions.initialQuestions)
+            .prompt(readmeQuestions)
             .then(function(data){
-                fs.writeFile("readmeData.json", JSON.stringify(data, null, 2), function(err){
+                for (const d in data) {
+                    formattedData.push(generateMarkdown(d, data[d]))
+                }
+                fs.writeFile("README.md", formattedData.join(""), function(err){
                     if (err) {
                         console.log(err);
                     }
-                    fs.readFile("readmeData.json", "utf8", function(err, data) {
-                        if (err) throw err;
-            
-                        let userResponse = JSON.parse(data);
-                        if (userResponse.content[0]) {
-                            createTableofContents(userResponse.content)
-                        }
-                    })
                 })
             })
         } catch (err) {
-            console.log(err);
+            console.log(err)
         }
-    }  
-
-
-
-function createTableofContents(data) {
-    console.log(data)
-}
-// async function writeToFile(fileName, data) {
-
-// }
-
-// function init() {
-
-// }
-
-// init();
+    }
