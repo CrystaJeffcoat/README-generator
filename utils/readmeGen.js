9c85ac878989c7aef546929918b2928e336700ef
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-07-04 12:04:09 -0400

    moved file

---
+++ utils/readmeGen.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+      type: "input",
+      name: "title",
+      message: "Project title? "
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "Project description? "
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "Installation: "
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "Usage: "
+    },
+    {
+        type: "input",
+        name: "Contributing",
+        message: "Contributions: "
+    },
+    {
+        type: "input",
+        name: "License",
+        message: "License: "
+    }
+]
